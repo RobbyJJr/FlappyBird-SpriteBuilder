532347b655e54b204606ab53dd35967cdf8559fd
Robert Eastmond <roberteastmond@Roberts-MacBook-Pro-2.local>
2014-06-24 14:12:06 -0600

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -7,13 +7,42 @@
 - (void)initialize
 {
     // your code here
+    character = (Character*)[CCBReader load:@"Character"];
+    [physicsNode addChild:character];
+    timeSinceObstacle = 0.0f;
 }
 
 -(void)update:(CCTime)delta
 {
+    //this function runs once each frame (60 frames per second)
+    //delta is the time between each run
     // put update code here
+    
+    //Increment the time since the last obstacle was added
+    timeSinceObstacle += delta; //delta = ~1/60th second
+    
+    if(timeSinceObstacle > 2.0f)
+    {
+        
+        //Add new Obstacle
+        [self addObstacle];
+        
+        //Reset the timer
+        timeSinceObstacle = 0.0f;
+    }
+    
+    
+    
 }
 
 // put new methods here
+
+-(void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event{
+    //called when someone touches the screen
+    //we need to have an action that calls this method
+    //looks like touchBegan is a built in function of the Cocos2D engine
+    [character flap];
+    
+}
 
 @end
